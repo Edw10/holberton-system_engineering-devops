5c029b0439bec6ce3cc3adfca9518e2d25290107
Edward Osorno <edward-1094@hotmail.com>
2020-05-08 05:10:15 +0000

    add

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -9,7 +9,7 @@ def recurse(subreddit, hot_list=[], after=""):
     user["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"
     data = requests.get(
-        "https://www.reddit.com/r/{}/hot.json?limit=50&after={}".format(
+        "https://www.reddit.com/r/{}/hot.json?limit=100&after={}".format(
             subreddit, after
         ),
         headers=user
