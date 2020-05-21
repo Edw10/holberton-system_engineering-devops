738423345281c1f0958bf4c79777987d2f8e88c5
Edward Osorno <edward-1094@hotmail.com>
2020-05-21 03:28:37 +0000

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 #correct the 500 error
 file_line { 'replace':
-  path      => '/var/www/html/wp-settings.php',
+  path    => '/var/www/html/wp-settings.php',
   replace => true,
-  line       => 'require_once( ABSPATH . WPINC . "/class-wp-locale.php" );',
-  match  => 'require_once( ABSPATH . WPINC . "/class-wp-locale.phpp" );'
+  line    => 'require_once( ABSPATH . WPINC . "/class-wp-locale.php" );',
+  match   => 'require_once( ABSPATH . WPINC . "/class-wp-locale.phpp" );'
 }
